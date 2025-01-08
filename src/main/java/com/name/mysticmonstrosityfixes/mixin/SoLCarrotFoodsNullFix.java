018ea2b8f7c51773a221de1a4afee1cbd3429d80
ton185 <174216122+ton185@users.noreply.github.com>
2025-01-08 12:17:49 +0000

    Create SoLCarrotFoodsNullFix.java

---
+++ src/main/java/com/name/mysticmonstrosityfixes/mixin/SoLCarrotFoodsNullFix.java
@@ -0,0 +1,28 @@
+package com.name.mysticmonstrosityfixes.mixin;
+
+import com.cazsius.solcarrot.client.FoodItems;
+import net.minecraft.client.resources.language.I18n;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.registries.ForgeRegistries;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Mixin(FoodItems.class)
+public abstract class SoLCarrotFoodsNullFix {
+    @Shadow
+    private static List<Item> foodsBeforeBlacklist;
+
+    @Inject(at = @At("HEAD"), method = "applyBlacklist", remap = false)
+    private static void nullFix(CallbackInfo ci) {
+        if (foodsBeforeBlacklist == null) {
+            foodsBeforeBlacklist = ForgeRegistries.ITEMS.getValues().stream().filter(Item::isEdible).sorted(Comparator.comparing((food) -> I18n.get(food.getDescriptionId() + ".name", new Object[0]))).collect(Collectors.toList());
+        }
+    }
+}
