1e17dc8f18a32040ce96189f638c6e4573635971
Mysticpasta1 <ethana5tc@outlook.com>
2025-09-03 22:54:15 -0500

    remove unneeded cache system, optimize fill biome command

--- src/main/java/com/name/mysticmonstrosityfixes/util/Entry.java
+++
@@ -1,7 +0,0 @@
-package com.name.mysticmonstrosityfixes.util;
-
-public final class Entry {
-        public long size;
-        public long crc32;
-        public long lastModified; // when we wrote it
-    }
