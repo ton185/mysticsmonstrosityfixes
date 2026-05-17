959730a5e6a1b7f21c0a27d2e03d3ba06a1e3e6b
ton185 <174216122+ton185@users.noreply.github.com>
2026-05-18 00:21:51 +0100

    try new fix

--- src/main/java/com/name/mysticmonstrosityfixes/mixin/SolarCraftShaderLoadCrashFix.java
+++ src/main/java/com/name/mysticmonstrosityfixes/mixin/SolarCraftShaderLoadCrashFix.java
@@ -1,18 +1,28 @@
 package com.name.mysticmonstrosityfixes.mixin;
 
-import com.finderfeed.solarcraft.client.rendering.shaders.post_chains.UniformPlusPlus;
 import com.finderfeed.solarcraft.content.blocks.render.RuneEnergyPylonRenderer;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.level.block.entity.BlockEntity;
+import com.mojang.blaze3d.platform.Window;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(RuneEnergyPylonRenderer.class)
 public class SolarCraftShaderLoadCrashFix {
-    @Inject(at = @At(value = "INVOKE", target = "Ljava/lang/Exception;printStackTrace()V"), method = "loadShader", cancellable = true, remap = false)
-    public void cancelIfErrored(BlockEntity tile, ResourceLocation LOC, UniformPlusPlus uniforms, CallbackInfo ci) {
-        ci.cancel();
+    @Redirect(at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/platform/Window;getScreenWidth()I"), method = "loadShader")
+    public int fixWidth(Window instance) {
+        int width = instance.getWidth();
+        if (width == 0) {
+            return 1;
+        }
+        return width;
+    }
+
+    @Redirect(at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/platform/Window;getScreenHeight()I"), method = "loadShader")
+    public int fixHeight(Window instance) {
+        int height = instance.getWidth();
+        if (height == 0) {
+            return 1;
+        }
+        return height;
     }
 }
