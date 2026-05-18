1bf23d7e27a3a901e553e161b049cd6b23ee101a
ton185 <174216122+ton185@users.noreply.github.com>
2026-05-18 01:50:14 +0100

    Update SolarCraftShaderLoadCrashFix.java

--- src/main/java/com/name/mysticmonstrosityfixes/mixin/SolarCraftShaderLoadCrashFix.java
+++ src/main/java/com/name/mysticmonstrosityfixes/mixin/SolarCraftShaderLoadCrashFix.java
@@ -1,16 +1,20 @@
 package com.name.mysticmonstrosityfixes.mixin;
 
-import com.finderfeed.solarcraft.client.rendering.shaders.post_chains.PostChainPlusUltra;
 import com.finderfeed.solarcraft.content.blocks.render.RuneEnergyPylonRenderer;
+import com.mojang.blaze3d.platform.Window;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(RuneEnergyPylonRenderer.class)
 public class SolarCraftShaderLoadCrashFix {
-    @Redirect(at = @At(value = "INVOKE", target = "Lcom/finderfeed/solarcraft/client/rendering/shaders/post_chains/PostChainPlusUltra;resize(II)V"), method = "loadShader")
-    public void dontResize(PostChainPlusUltra instance, int i, int i2) {
-        // no-op the resize
-        return;
+    @Redirect(at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/platform/Window;getScreenWidth()I"), method = "loadShader")
+    public int fixWidth(Window instance) {
+        return instance.getWidth();
+    }
+
+    @Redirect(at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/platform/Window;getScreenHeight()I"), method = "loadShader")
+    public int fixHeight(Window instance) {
+        return instance.getHeight();
     }
 }
