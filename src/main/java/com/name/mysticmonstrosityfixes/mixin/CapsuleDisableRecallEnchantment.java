460ebd433c3b18fcc5a1266d4dcea4bf52753b44
ton185 <174216122+ton185@users.noreply.github.com>
2026-06-25 00:22:33 +0100

    Update CapsuleDisableRecallEnchantment.java

--- src/main/java/com/name/mysticmonstrosityfixes/mixin/CapsuleDisableRecallEnchantment.java
+++ src/main/java/com/name/mysticmonstrosityfixes/mixin/CapsuleDisableRecallEnchantment.java
@@ -2,6 +2,7 @@ package com.name.mysticmonstrosityfixes.mixin;
 
 import capsule.enchantments.RecallEnchant;
 import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 
@@ -11,7 +12,8 @@ public class CapsuleDisableRecallEnchantment {
      * @author ton185
      * @reason disable the recall enchantment as it causes lag
      */
-    @Overwrite
+    @SubscribeEvent
+    @Overwrite(remap = false)
     public static void onWorldTickEvent(TickEvent.LevelTickEvent wte) {
     }
 }
